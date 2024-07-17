3ea371f699dbb5e8ed53ee2964d288efeaf2aff9
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-07-17 14:50:01 +0530

    serialize the animal object.

--- src/main/java/lk/ijse/gdse68/Main.java
+++ src/main/java/lk/ijse/gdse68/Main.java
@@ -1,7 +1,17 @@
 package lk.ijse.gdse68;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Animal animal = new Animal("Cat", 2);
+
+        // serialization
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("animal.ser"))) {
+            oos.writeObject(animal);
+            System.out.println("Serialization successful : " + animal);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
