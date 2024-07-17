93af11c69ddd56680f241ced591e0f6704b6e61f
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-07-17 14:43:18 +0530

    custom serialization & deserialization logics implemented..

--- src/main/java/lk/ijse/gdse68/Animal.java
+++ src/main/java/lk/ijse/gdse68/Animal.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serial;
-import java.io.Serializable;
+import java.io.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,4 +15,18 @@ public class Animal implements Serializable {
     private static final long serialVersionUID = 1L;
     private String name;
     private int age;
+
+    // serialization logic
+    @Serial
+    private void writeObject(ObjectOutputStream oos) throws IOException {
+        oos.defaultWriteObject();
+        oos.writeInt(age);
+    }
+
+    // deserialization logic
+    @Serial
+    private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
+        ois.defaultReadObject();
+        age = ois.readInt();
+    }
 }
